48f555947fb30b3e504783186b39503ac9367c51
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2018-02-15 16:28:12 -0800

    Non-Json get extension.

--- Data/Json/IRestClientExtensions.cs
+++ Data/Json/IRestClientExtensions.cs
@@ -8,6 +8,51 @@ namespace HD
 {
   public static class IRestClientExtensions
   {
+    public static async Task<string> AsyncDownload(
+      this IRestClient restClient,
+      string url,
+      Method method = Method.GET,
+      (string key, string value)[] headers = null)
+    {
+      Debug.Assert(url.StartsWith("/") == false);
+
+      return await restClient.HttpRequestAsync($"/{url}", null, method, headers);
+    }
+
+    static async Task<string> HttpRequestAsync(
+      this IRestClient restClient,
+      string url,
+      string requestJson,
+      Method method,
+      (string key, string value)[] headers)
+    {
+      IRestRequest request = new RestRequest(url, method);
+
+      // if you want to use fiddler
+      //restClient.Proxy = new WebProxy("localhost", 8888); // IP, Port.
+
+      request.AddHeader("Content-Type", "application/json"); //not needed, right?
+      request.AddHeader("Accept", "application/json");
+
+      if (headers != null)
+      {
+        for (int i = 0; i < headers.Length; i++)
+        {
+          (string key, string value) = headers[i];
+          request.AddHeader(key, value);
+        }
+      }
+
+      if (requestJson != null)
+      {
+        request.RequestFormat = DataFormat.Json;
+        request.AddParameter("application/json", requestJson, ParameterType.RequestBody);
+      }
+
+      IRestResponse response = await restClient.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+
     public static async Task<T> AsyncDownload<T>(
       this IRestClient restClient,
       string url,
